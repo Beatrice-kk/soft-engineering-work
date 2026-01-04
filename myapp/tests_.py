a0c27ca08e8eaf06f4d0dbb50c3ebdf133d8b221
WindyKelp <162412300+Beatrice-kk@users.noreply.github.com>
2026-01-04 16:48:49 +0800

    数据库修改

--- myapp/tests_.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-from myapp.models import PassengerAccount, Passenger, Arrange, Ticket
-
--- vue/src/utils/MiddleUtil.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-export default new Vue;
-/*
-两个vue界面之间的函数调用
-中间js*/
