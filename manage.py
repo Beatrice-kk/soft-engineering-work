e315ebfab90f28e4b9c65b2d8970ccc203361374
WindyKelp <162412300+Beatrice-kk@users.noreply.github.com>
2026-01-04 15:06:18 +0800

    modify

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+# manage.py
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flight.settings')
@@ -15,7 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)#
 
 
 if __name__ == '__main__':
